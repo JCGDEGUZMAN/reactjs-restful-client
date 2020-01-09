8a27aa0d226654e56d7809543fa258d51f410792
JCDEGUZMAN <julius.d@cloudwalkdigital.com>
2020-01-09 14:38:09 +0800

    create new profile page modal

--- src/modules/profile/new_profile/index.js
+++ src/modules/profile/new_profile/index.js
@@ -1,12 +1,40 @@
 import './index.css';
 import React, {Component} from 'react';
+import { Form, Modal, Input } from 'antd';
 
 class NewProfile extends Component {
     render(){
+        const { visible, onCancel, onCreate, form } = this.props;
+        const { getFieldDecorator } = form;
         return(
-            <div>Hello.....</div>
+            <Modal
+                className="new-profile-modal"
+                visible={true}
+                title="Create a New Profile"
+                okText="Create"
+                onCancel={onCancel}
+                onOk={onCreate}
+            >
+                <Form layout="vertical">
+                    <Form.Item label="First Name">
+                        {getFieldDecorator('fname', {
+                            rules: [{ required: true, message: 'Please input first name!' }],
+                        })(<Input />)}
+                    </Form.Item>
+                    <Form.Item label="Middle Name">
+                        {getFieldDecorator('mname', {
+                            rules: [{ required: true, message: 'Please input middle name!' }],
+                        })(<Input />)}
+                    </Form.Item>
+                    <Form.Item label="Last Name">
+                        {getFieldDecorator('lname', {
+                            rules: [{ required: true, message: 'Please input last name!' }],
+                        })(<Input />)}
+                    </Form.Item>
+                </Form>
+            </Modal>
         );
     }
 }
 
-export default NewProfile;
+export default Form.create()(NewProfile);
