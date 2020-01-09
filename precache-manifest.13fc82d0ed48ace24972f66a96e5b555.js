3156430c9c24696860f86b855d88921564d59406
JCDEGUZMAN <julius.d@cloudwalkdigital.com>
2020-01-09 11:52:22 +0800

    Updates

---
+++ precache-manifest.13fc82d0ed48ace24972f66a96e5b555.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0471fce1a5b736616c358b5ed45a447",
+    "url": "/reactjs-restful-client/index.html"
+  },
+  {
+    "revision": "15e2bb99f76d983f09d2",
+    "url": "/reactjs-restful-client/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "2782ea27d827581340df",
+    "url": "/reactjs-restful-client/static/css/main.871c5aab.chunk.css"
+  },
+  {
+    "revision": "15e2bb99f76d983f09d2",
+    "url": "/reactjs-restful-client/static/js/2.002c1aab.chunk.js"
+  },
+  {
+    "revision": "2782ea27d827581340df",
+    "url": "/reactjs-restful-client/static/js/main.c3895ffa.chunk.js"
+  },
+  {
+    "revision": "684b014ce40f839ec7fd",
+    "url": "/reactjs-restful-client/static/js/runtime-main.47b2756e.js"
+  },
+  {
+    "revision": "e3062d8da98d4c90af3ad8613ca226e7",
+    "url": "/reactjs-restful-client/static/media/Copperplate_Gothic_Bold_Regular.e3062d8d.ttf"
+  }
+]);
