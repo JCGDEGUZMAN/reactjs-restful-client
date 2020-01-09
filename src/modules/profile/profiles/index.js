7372e786f9a44f40f0a4566b02b2c99a2e16f74e
JCDEGUZMAN <julius.d@cloudwalkdigital.com>
2020-01-09 11:05:28 +0800

    added edit, delete, add button

--- src/modules/profile/profiles/index.js
+++ src/modules/profile/profiles/index.js
@@ -1,76 +1,110 @@
 import React, { Component } from 'react';
 import './index.css';
-import { Layout, Table, Row, Col } from 'antd';
+import { Layout, Table, Icon } from 'antd';
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Content } = Layout;
+
+class NewProfile extends Component {
+    render(){
+        return(
+                <Icon className="new-profile-icon" type="form" />
+        );
+    }
+}
 
 const tableColumns = [
     {
-        title: 'First Name',
+        title: 'FIRST NAME',
         dataIndex: 'fname',
     },
     {
-        title: 'Middle Name',
+        title: 'MIDDLE NAME',
         dataIndex: 'mname',
     },
     {
-        title: 'Last Name',
+        title: 'LAST NAME',
         dataIndex: 'lname',
+    },
+    {
+        title: <NewProfile />,
+        dataIndex: "id",
+        render: (a, row) => <Actions profileId={a} profileData={row} />
     },
 ];
 
 const tableData = [
     {
+        id: 1,
         fname: 'Julius',
         mname: 'Guevarra',
         lname: 'De Guzman',
     },
     {
+        id: 2,
         fname: 'Lisalea',
         mname: 'Angeles',
         lname: 'Santiago',
     },
     {
+        id: 3,
         fname: 'Julius',
         mname: 'Guevarra',
         lname: 'De Guzman',
     },
     {
+        id: 4,
         fname: 'Lisalea',
         mname: 'Angeles',
         lname: 'Santiago',
     },
     {
+        id: 5,
         fname: 'Julius',
         mname: 'Guevarra',
         lname: 'De Guzman',
     },
     {
+        id: 6,
         fname: 'Lisalea',
         mname: 'Angeles',
         lname: 'Santiago',
     },
     {
+        id: 7,
         fname: 'Julius',
         mname: 'Guevarra',
         lname: 'De Guzman',
     },
     {
+        id: 8,
         fname: 'Lisalea',
         mname: 'Angeles',
         lname: 'Santiago',
     },
     {
+        id: 9,
         fname: 'Julius',
         mname: 'Guevarra',
         lname: 'De Guzman',
     },
     {
+        id: 10,
         fname: 'Lisalea',
         mname: 'Angeles',
         lname: 'Santiago',
     },
 ];
+
+class Actions extends Component {
+    render(){
+        return(
+        <div>
+            <Icon className="action-icons" type="edit" />
+            <Icon className="action-icons" type="delete" />
+        </div>
+        );
+    }
+}
 
 class Profiles extends Component {
     render(){
