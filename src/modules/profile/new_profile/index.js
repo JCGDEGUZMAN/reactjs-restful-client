7ec51b8fdb499802a18bad7bc7854cfb44d0d8ed
JCDEGUZMAN <julius.d@cloudwalkdigital.com>
2020-01-09 15:06:37 +0800

    new profile page done

--- src/modules/profile/new_profile/index.js
+++ src/modules/profile/new_profile/index.js
@@ -1,38 +1,42 @@
 import './index.css';
 import React, {Component} from 'react';
-import { Form, Modal, Input } from 'antd';
+import { Form, Modal, Input, Row, Col } from 'antd';
 
 class NewProfile extends Component {
     render(){
         const { visible, onCancel, onCreate, form } = this.props;
         const { getFieldDecorator } = form;
         return(
-            <Modal
-                className="new-profile-modal"
-                visible={true}
-                title="Create a New Profile"
-                okText="Create"
-                onCancel={onCancel}
-                onOk={onCreate}
-            >
-                <Form layout="vertical">
-                    <Form.Item label="First Name">
-                        {getFieldDecorator('fname', {
-                            rules: [{ required: true, message: 'Please input first name!' }],
-                        })(<Input />)}
-                    </Form.Item>
-                    <Form.Item label="Middle Name">
-                        {getFieldDecorator('mname', {
-                            rules: [{ required: true, message: 'Please input middle name!' }],
-                        })(<Input />)}
-                    </Form.Item>
-                    <Form.Item label="Last Name">
-                        {getFieldDecorator('lname', {
-                            rules: [{ required: true, message: 'Please input last name!' }],
-                        })(<Input />)}
-                    </Form.Item>
-                </Form>
-            </Modal>
+            <Row type="flex" align="middle" justify="center">
+                <Col span={24}>
+                    <Modal
+                        className="new-profile-modal"
+                        visible={true}
+                        title="Create a New Profile"
+                        okText="Create"
+                        onCancel={onCancel}
+                        onOk={onCreate}
+                    >
+                        <Form layout="vertical">
+                            <Form.Item label="First Name">
+                                {getFieldDecorator('fname', {
+                                    rules: [{ required: true, message: 'Please input first name!' }],
+                                })(<Input />)}
+                            </Form.Item>
+                            <Form.Item label="Middle Name">
+                                {getFieldDecorator('mname', {
+                                    rules: [{ required: true, message: 'Please input middle name!' }],
+                                })(<Input />)}
+                            </Form.Item>
+                            <Form.Item label="Last Name">
+                                {getFieldDecorator('lname', {
+                                    rules: [{ required: true, message: 'Please input last name!' }],
+                                })(<Input />)}
+                            </Form.Item>
+                        </Form>
+                    </Modal>
+                </Col>
+            </Row>
         );
     }
 }
